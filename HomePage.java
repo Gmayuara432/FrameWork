6420098444fb79e1cb252185e9dc0f3a5bc8511a
Gmayuara432 <129709145+Gmayuara432@users.noreply.github.com>
2023-07-28 20:26:10 +0530

    Create HomePage.java

---
+++ HomePage.java
@@ -0,0 +1,27 @@
+package com.ActiTime.pom;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage {
+
+
+		@FindBy(id="logoutLink")
+		private WebElement lgOut;
+		
+		@FindBy(xpath="//div[text()='Tasks']")
+		private WebElement tasksTab;
+		
+		public HomePage(WebDriver driver) {
+			PageFactory.initElements(driver, this);
+		}
+		public void setLogOut() {
+			lgOut.click();
+		}
+		public void setTasksTab() {
+			tasksTab.click();
+		}
+
+}
